a469e8a25a28b9890de7ca823761ab3da3432c95
Jai Malani <71269258+jai38@users.noreply.github.com>
2020-12-09 18:40:19 +0530

    updated mongoose

--- app.js
+++ app.js
@@ -30,5 +30,3 @@ app.use('/login/paymentUndone',require('./routes/Login/paymentUndone'));
 app.use('/login/payWithPin',require('./routes/Login/payWithPin'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
-
-module.exports = app;
