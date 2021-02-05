443ae29bab73479537082265c34643a3a6a87285
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-02-06 02:09:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const db = process.env.DB_URL;
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("MongoDB connected".repeat(100));
+    console.log("MongoDB connected");
   })
   .catch((err) => {
     console.log(err);
