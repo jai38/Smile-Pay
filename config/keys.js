1b60b4827d82130ec4e501152d211443196e4fd9
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-02-05 21:52:14 +0530

    removed keys

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // MongoURI: 'mongodb+srv://jai_m:Jai2001@cluster0.f2u1l.mongodb.net/jai?retryWrites=true&w=majority'
-  MongoURI:
-    "mongodb+srv://jai_m:Jai2001@cluster0.f2u1l.mongodb.net/jai?retryWrites=true&w=majority",
-};
