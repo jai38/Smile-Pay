0571434ea1d7c34c15ea7d79ea5b9fbee16ce4a9
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-02-16 22:18:38 +0530

    navigator.mediaDevices.getUserMedia

--- clientJS/Signup/face.js
+++ clientJS/Signup/face.js
@@ -10,7 +10,7 @@ Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri("../modelsFace"),
 ]).then(startVideo);
 function startVideo() {
-  navigator.getUserMedia(
+  navigator.mediaDevices.getUserMedia(
     { video: {} },
     (stream) => (video.srcObject = stream),
     (err) => console.error(err)
